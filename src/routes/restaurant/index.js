e500fba1cbd7e43d50be794f5a865164e7c3fb9d
Gregorio Lucena <gregoriolucena91@gmail.com>
2021-07-11 20:30:02 -0400

    CRUD Restaurant Address

--- src/routes/restaurant/index.js
+++ src/routes/restaurant/index.js
@@ -1,60 +1,85 @@
 export default {
-    routes: [
-        // restaunrat types
-        {
-            name: "Restaurant Type",
-            path: "restaurant/restaurant-type",
-            component: () =>
-                import("../../views/modules/restaurant/restaurantType"),
-        },
-        {
-            name: "Restaurant Type Register",
-            path: "restaurant/restaurant-type/register",
-            component: () =>
-                import("../../views/modules/restaurant/restaurantType/Register"),
-        },
-        {
-            name: "Restaurant Type Edit",
-            path: "restaurant/restaurant-type/edit/:id",
-            props: true,
-            component: () =>
-                import("../../views/modules/restaurant/restaurantType/Register"),
-        },
-        {
-            name: 'Restaurant',
-            path: "restaurant/restaurant",
-            component: () => import("../../views/modules/restaurant/restaurant")
-        },
-        {
-            name: 'Restaurant Register',
-            path: "restaurant/restaurant/register",
-            component: () => import("../../views/modules/restaurant/restaurant/register")
-        },
-        {
-            name: 'Restaurant Edit',
-            path: "restaurant/restaurant/edit/:id",
-            props: true,
-            component: () => import("../../views/modules/restaurant/restaurant/register")
-        }
+  routes: [
+    // routes for Type Restaurant
+    {
+      name: "Restaurant Type",
+      path: "restaurant/restaurant-type",
+      component: () => import("../../views/modules/restaurant/restaurantType"),
+    },
+    {
+      name: "Restaurant Type Register",
+      path: "restaurant/restaurant-type/register",
+      component: () =>
+        import("../../views/modules/restaurant/restaurantType/Register"),
+    },
+    {
+      name: "Restaurant Type Edit",
+      path: "restaurant/restaurant-type/edit/:id",
+      props: true,
+      component: () =>
+        import("../../views/modules/restaurant/restaurantType/Register"),
+    },
+    // routes for Restaurant
+    {
+      name: "Restaurant",
+      path: "restaurant/restaurant",
+      component: () => import("../../views/modules/restaurant/restaurant"),
+    },
+    {
+      name: "Restaurant Register",
+      path: "restaurant/restaurant/register",
+      component: () =>
+        import("../../views/modules/restaurant/restaurant/register"),
+    },
+    {
+      name: "Restaurant Edit",
+      path: "restaurant/restaurant/edit/:id",
+      props: true,
+      component: () =>
+        import("../../views/modules/restaurant/restaurant/register"),
+    },
+    // routes for Address Restaurant
+    {
+      name: "Address Restaurant",
+      path: "restaurant/restaurant-address",
+      component: () =>
+        import("../../views/modules/restaurant/restaurantAddress"),
+    },
+    {
+      name: "Address Restaurant Register",
+      path: "restaurant/restaurant-address/register",
+      component: () =>
+        import("../../views/modules/restaurant/restaurantAddress/Register"),
+    },
+    {
+      name: "Address Restaurant Edit",
+      path: "restaurant/restaurant-address/edit/:id",
+      props: true,
+      component: () =>
+        import("../../views/modules/restaurant/restaurantAddress/Register"),
+    },
+  ],
+
+  menu: {
+    group: "/restaurant",
+    title: "Restaurantes",
+    icon: "mdi-food",
+    children: [
+      {
+        title: "Tipo de Restaurante",
+        icon: "mdi-comment-outline",
+        to: "restaurant-type",
+      },
+      {
+        title: "Restaurantes",
+        icon: "mdi-store",
+        to: "restaurant",
+      },
+      {
+        title: "Dirección Restaurante",
+        icon: "mdi-store",
+        to: "restaurant-address",
+      },
     ],
-
-
-
-    menu: {
-        group: '/restaurant',
-        title: 'Restaurantes',
-        icon: 'mdi-food',
-        children: [
-            {
-                title: "Tipo de Restaurante",
-                icon: "mdi-comment-outline",
-                to: "restaurant-type",
-            },
-            {
-                title: "Restaurantes",
-                icon: "mdi-store",
-                to: "restaurant",
-            }
-        ]
-    }
+  },
 };
