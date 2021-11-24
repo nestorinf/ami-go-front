f155347862c4b496cc0c3c45cb64ef3744eb07f7
Carlos Lezama <clezama1991@gmail.com>
2021-11-24 11:13:22 -0400

    secure deletes

--- src/views/modules/products/classification/index.vue
+++ src/views/modules/products/classification/index.vue
@@ -31,6 +31,11 @@
       @handler-dialog-confirm="removeButton"
       :message="messageDialog"
     ></DialogConfirm>
+    <SnackBar
+        :text="textSnackBar"
+        ref="snackBarRef"
+        :snackbar="true"
+    ></SnackBar>
   </v-container>
 </template>
 
@@ -40,12 +45,14 @@ import ButtonRegister from "../../components/ButtonRegister";
 import ButtonCrudTable from "../../components/ButtonCrudTable";
 import DialogConfirm from "../../components/DialogConfirm";
 import { mapGetters, mapActions } from "vuex";
+import SnackBar from "@/views/modules/components/SnackBar";
 
 export default {
   name: "Classification",
   components: {
     DataTable,
     DialogConfirm,
+    SnackBar,
   },
 
   data: () => ({
@@ -64,6 +71,7 @@ export default {
       },
     ],
     messageDialog: "",
+    textSnackBar: "",
     ButtonRegister: ButtonRegister,
     ButtonCrud: ButtonCrudTable,
     titleForm: "Clasificación",
@@ -110,8 +118,18 @@ export default {
       this.idDelete = item.id;
       this.$refs.DialogConfirm.changeStateDialog(true);
     },
-    removeButton() {
-      this.removeClassification(this.idDelete);
+    removeButton() { 
+      this.removeClassification(this.idDelete)
+      .then((result) => {
+          if (result) {
+          this.$refs.snackBarRef.changeStatusSnackbar(true);
+          this.textSnackBar = "Eliminado existosamente!";
+          }
+      })
+      .catch((result) => {       
+          this.$refs.snackBarRef.changeStatusSnackbar(true);
+          this.textSnackBar = result.response.data.message;
+      });
       this.$refs.DialogConfirm.changeStateDialog(false);
     },
   },
